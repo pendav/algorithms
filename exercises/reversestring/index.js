75fbe2ebab27eebcde7754b7daf439b2e6a623b7
david.pena <david.pena@globant.com>
2019-02-02 19:29:27 -0600

    Add three solutions to the first exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((rev, char) => 
+       char + rev
+  , '');
+}
 
 module.exports = reverse;
+
+// return str
+//   .split("")
+//   .reverse()
+//   .join("");
+
+// let reversed = '';
+// for (let character of str) {
+//     reversed = character + reversed;
+// }
+// return reversed;
