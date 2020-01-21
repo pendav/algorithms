517bd9aca4ee4bab785a16c137679de04103b006
pendav <daguilar85@gmail.com>
2020-01-20 20:20:11 -0600

    Add a solution to the fibonaci exercise with memoize

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,42 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if(cache[args]){
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    }
+}
+
+function slowfib(n) {
+    if (n < 2) {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+
+}
+
+const fib = memoize(slowfib);
 
 module.exports = fib;
+
+// function fib(n) {
+//     const result = [0,1];
+
+//     for(let i=2; i<= n; i++) {
+//         const a = result[i -1];
+//         const b = result[i -2];
+
+//         result.push(a+b);
+
+//     }
+//     return result[n];
+// }
